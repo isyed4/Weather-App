273d95cdc473c259d402f839f7ecd8abdc4fbcb3
Imran Syed <imran@Imrans-MacBook-Air.local>
2023-03-11 14:54:56 -0600

    added api call

--- src/App.js
+++ src/App.js
@@ -6,9 +6,27 @@ function App() {
 const [findCoord, setFindCoord] = useState("")
 const [longitude, setLongitude] = useState("")
 const [latitude, setLatitude] = useState("")
+const [coordinates, setCoordinates] = useState([])
 
+const apiKey = "4d960f6c1e1c4f1c9cfd564d463b35a1"
 
+const handleCoordChange = (event) => {
+  setFindCoord(event.target.value)
+}
+const handleLongChange = (event) => {
+  setLongitude(event.target.value)
+}
+const handleLatChange = (event) => {
+  setLatitude(event.target.value)
+}
 
+const handleLocationSubmit = (event) => {
+  event.preventDefault();
+
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${findCoord}&limit=5&appid=${apiKey}`)
+  .then((response) => response.json())
+  .then((data) => setCoordinates(data.response))
+}
 
 
   return (
@@ -16,14 +34,19 @@ const [latitude, setLatitude] = useState("")
       <h1>Global Current Weather</h1>
       <form>
         <label>Enter City Name for Longitude and Latitude Values</label>
-        <input type="text" placeholder=''/>
-        <p>text</p>
-        <br />
-        <label>Enter Longitutde and Latitude Values for Weather</label>
-        <input type="text" placeholder='longitude' />
-        <input type="text" placeholder="latitude"/>
-        <p></p>
+        <input type="text" onChange={handleCoordChange} value={findCoord} placeholder='City Name'/>
+        <input type="button" value='enter'/>
       </form>
+        <p></p>
+        <br />
+        <form>
+          <label>Enter Longitutde and Latitude Values for Weather</label>
+          <input type="text" onChange = {handleLongChange} value={longitude} placeholder='longitude' />
+          <input type="text" onChange = {handleLatChange} value={latitude} placeholder="latitude"/>
+          <input type="button" value="enter"/>
+        </form>
+        <p></p>
+     
     </div>
   );
 }
