8b13655e7a348cad4f36ff33d0a0ee4cb457dced
Imran Syed <imran@Imrans-MacBook-Air.local>
2023-03-11 15:36:36 -0600

    successful api retrieval and display

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 function App() {
 
 const [findCoord, setFindCoord] = useState("")
+const [limit, setLimit] = useState("")
 const [longitude, setLongitude] = useState("")
 const [latitude, setLatitude] = useState("")
 const [coordinates, setCoordinates] = useState([])
@@ -12,6 +13,9 @@ const apiKey = "4d960f6c1e1c4f1c9cfd564d463b35a1"
 
 const handleCoordChange = (event) => {
   setFindCoord(event.target.value)
+}
+const handleLimitChange = (event) => {
+  setLimit(event.target.value)
 }
 const handleLongChange = (event) => {
   setLongitude(event.target.value)
@@ -23,21 +27,37 @@ const handleLatChange = (event) => {
 const handleLocationSubmit = (event) => {
   event.preventDefault();
 
-  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${findCoord}&limit=5&appid=${apiKey}`)
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${findCoord}&limit=${limit}&appid=${apiKey}`)
   .then((response) => response.json())
-  .then((data) => setCoordinates(data.response))
+  .then((data) => setCoordinates(data))
+
+  setFindCoord("");
+  console.log(coordinates);
 }
+
+const cityCoords = coordinates.map((item) => {
+  return(
+    <ul>
+      <li>{item.name}</li>
+      <li>Latitude{item.lat}</li>
+      <li>Longitude{item.lon}</li>
+      <li>Country{item.country}</li>
+      <li>State{item.state}</li>
+    </ul>
+  )
+})
 
 
   return (
     <div className="App">
       <h1>Global Current Weather</h1>
-      <form>
+      <form onSubmit={handleLocationSubmit}>
         <label>Enter City Name for Longitude and Latitude Values</label>
         <input type="text" onChange={handleCoordChange} value={findCoord} placeholder='City Name'/>
-        <input type="button" value='enter'/>
+        <input type="text" value={limit} onChange={handleLimitChange} placeholder='limit of results' />
+        <input type="submit" value='enter'/>
       </form>
-        <p></p>
+      <div>{cityCoords}</div>
         <br />
         <form>
           <label>Enter Longitutde and Latitude Values for Weather</label>
