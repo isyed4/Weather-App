b9d9a3ee0fa27d53585e0706bee71a451440878d
Imran Syed <imran@Imrans-MacBook-Air.local>
2023-03-11 14:25:33 -0600

    added title,forms, and dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Global Current Weather</h1>
+      <form>
+        <label>Enter City Name for Longitude and Latitude Values</label>
+        <input type="text" placeholder=''/>
+        <p>text</p>
+        <br />
+        <label></label>
+        <input type="text" />
+      </form>
     </div>
   );
 }
